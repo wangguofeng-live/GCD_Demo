c572d6fc1f6fff284f4db46a2b8be6919dd1ea2f
wangguofeng-live <wangguofeng@live.cn>
2018-01-04 14:42:44 +0800

    dispatch_group的使用

--- GCD_Demo/VM/GroupVM.m
+++ GCD_Demo/VM/GroupVM.m
@@ -16,54 +16,101 @@
 }
 
 - (void)testVM {
-    [self dispatchGroup];
+    
+//    [self dispatchWork];
+    
+//    [self groupSync2];
+    
+    [self dispatchNewwork];
 }
 
--(void)dispatchGroup {
+#pragma mark -
+-(void)dispatchWork {
+    dispatch_queue_t queue_t = dispatch_queue_create("gcd_demo_queue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_group_t group_t = dispatch_group_create();
+    
+    dispatch_group_async(group_t, queue_t, ^{
+        sleep(2);
+        NSLog(@"任务1完成");
+    });
+    
+    dispatch_group_async(group_t, queue_t, ^{
+        sleep(3);
+        NSLog(@"任务2完成");
+        
+    });
+    
+    dispatch_group_async(group_t, queue_t, ^{
+        NSLog(@"任务3完成");
+    });
+    
+    //任务完成方式一
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        dispatch_group_wait(group_t, DISPATCH_TIME_FOREVER);
+        NSLog(@"任务完成通知 Wait");
+    });
+    
+    //任务完成方式二
+    dispatch_group_notify(group_t, dispatch_get_main_queue(), ^{
+        NSLog(@"任务完成通知 Notify");
+    });
+    
+}
+
+- (void)groupSync2
+{
+
+    dispatch_queue_t queue_t = dispatch_queue_create("gcd_demo_queue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue_global_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_group_t group_t = dispatch_group_create();
+    
+    dispatch_group_async(group_t, queue_t, ^{
+        sleep(2);
+        NSLog(@"任务1完成");
+    });
+    
+    dispatch_group_async(group_t, queue_t, ^{
+        sleep(3);
+        NSLog(@"任务2完成");
+    });
+
+    //任务3 在系统的线程中执行
+    dispatch_group_async(group_t, queue_global_t, ^{
+        sleep(5);
+        NSLog(@"任务3完成");
+    });
+    
+    dispatch_group_notify(group_t, dispatch_get_main_queue(), ^(){
+        NSLog(@"任务完成通知");
+    });
+}
+
+#pragma mark  -
+
+-(void)dispatchNewwork {
     dispatch_queue_t queue_t = dispatch_queue_create("gcd_demo_queue", DISPATCH_QUEUE_CONCURRENT);
     dispatch_group_t group_t = dispatch_group_create();
     
     dispatch_group_enter(group_t);
-    dispatch_group_async(group_t, queue_t, ^{
-        
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            sleep(5);
-            
-            NSLog(@"网络任务1完----%@", [NSThread currentThread]);
-            dispatch_group_leave(group_t);
-        });
-        sleep(2);
-        NSLog(@"任务1完成---%@", [NSThread currentThread]);
+    dispatch_async(queue_t, ^{
+        sleep(5);
+        NSLog(@"网络任务1完成");
+        dispatch_group_leave(group_t);
     });
     
-//    dispatch_group_wait(group_t, DISPATCH_TIME_FOREVER);
     dispatch_group_enter(group_t);
-    dispatch_group_async(group_t, queue_t, ^{
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            sleep(5);
-            
-            NSLog(@"网络任务2完成----%@", [NSThread currentThread]);
-            dispatch_group_leave(group_t);
-        });
-        sleep(2);
-        NSLog(@"任务2完成---%@", [NSThread currentThread]);
-        
+    dispatch_async(queue_t, ^{
+        sleep(3);
+        NSLog(@"网络任务2完成");
+        dispatch_group_leave(group_t);
     });
     
-//     (group_t, DISPATCH_TIME_FOREVER);
     dispatch_group_enter(group_t);
-    dispatch_group_async(group_t, queue_t, ^{
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            sleep(5);
-            
-            NSLog(@"网络任务3完成 --- %@", [NSThread currentThread]);
-            dispatch_group_leave(group_t);
-        });
-        sleep(2);
-        NSLog(@"任务3完成---- %@", [NSThread currentThread]);
+    dispatch_async(queue_t, ^{
+        sleep(4);
+        NSLog(@"网络任务3完成");
+        dispatch_group_leave(group_t);
     });
-    
-    
     
     dispatch_group_notify(group_t, dispatch_get_main_queue(), ^{
         NSLog(@"任务完成通知");
